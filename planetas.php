b334974df6457a9aeb6f6c2cf3c4f8f01501ad84
Alexis Bejarano <aletzisbejaranov@hotmail.com>
2024-10-07 17:35:31 -0700

    Primer commit

---
+++ planetas.php
@@ -0,0 +1,26 @@
+
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <!-- FONTS -->
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Protest+Strike&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="style/style.css">
+
+</head>
+<body>
+    <div class="container">
+        <!-- EXAMPLE <h1>Planetas del Sistema Solar <?php echo $val1; ?></h1> -->
+        <h1>Planetas del Sistema Solar</h1>
+    </div>
+</body>
+</html>
